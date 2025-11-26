fde9813124e6dc20df9d31ab8bea82ff789760e8
Michael Simiyu <michaelsimiyu54@gmail.com>
2025-11-26 15:35:26 +0300

    developing sliding layout feature

--- app/src/main/java/com/example/myapplication/OnBoardingActivity.java
+++ app/src/main/java/com/example/myapplication/OnBoardingActivity.java
@@ -1,24 +1,93 @@
 package com.example.myapplication;
 
+import static androidx.compose.ui.semantics.SemanticsPropertiesKt.setText;
+
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
 
 public class OnBoardingActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+    Button btn;
+    SliderAdapter sliderAdapter;
+    TextView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //hide status bar
+        getWindow().setFlags(WindowManager).LayoutParams.FLAG.FULLSCREEN, WindowManager.FLAG_FULLSCREEN;
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_on_boarding);
+
+        //hide toolbar
+        getSupportActionBar().hide();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        viewPager = findViewById(R.id.slider);
+        dotsLayout = findViewById(R.id.dots);
+        btn = findViewById(R.id.get_started_btn);
+        addDots(0);
+
+        //Call Adapter
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+    }
+
+    private void addDots(int position) {
+
+        dots = new TextView[3];
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.pink));
+        }
+
+    }
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+            addDots(position);
+
+            if (position == 0) {
+
+            }
+
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
     }
 }
